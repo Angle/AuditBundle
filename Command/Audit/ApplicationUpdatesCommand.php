29e889adbc87f3d4ab6377eff7f921cc816531f7
Edmundo Fuentes <me@edmundofuentes.com>
2024-05-24 16:28:43 -0600

    implement ApplicationUpdates audit

--- Command/Audit/ApplicationUpdatesCommand.php
+++ Command/Audit/ApplicationUpdatesCommand.php
@@ -2,6 +2,8 @@
 
 namespace Angle\AuditBundle\Command\Audit;
 
+use Angle\AuditBundle\Utility\PeriodUtility;
+use Angle\AuditBundle\Utility\ReportUtility;
 use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
 use Doctrine\Persistence\ManagerRegistry;
@@ -13,15 +15,21 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ApplicationUpdatesCommand extends Command
 {
     protected static $defaultName = 'angle:audit:application-updates';
 
+    /** @var ParameterBagInterface $params */
+    private $params;
+
     const DEBUG = false;
 
-    public function __construct()
+    public function __construct(ParameterBagInterface $params)
     {
+        $this->params = $params;
+
         parent::__construct();
     }
 
@@ -31,6 +39,8 @@ class ApplicationUpdatesCommand extends Command
     protected function configure()
     {
         $this
+            ->addArgument('year', InputArgument::REQUIRED, 'Evaluation year (YYYY)')
+            ->addArgument('month', InputArgument::REQUIRED, 'Evaluation month (MM)')
             ->setDescription('Audit Report: application updates applied during the period');
     }
 
@@ -38,7 +48,59 @@ class ApplicationUpdatesCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $io->title($this->getDescription());
+        ReportUtility::printStartTimestamp($io);
 
+        $year   = intval($input->getArgument('year'));
+        $month  = intval($input->getArgument('month'));
+        $periodString = PeriodUtility::periodStringFromYearAndMonth($year, $month);
+
+        $output->writeln('<info>Report Evaluation Period:</info> ' . $periodString . PHP_EOL);
+
+
+        // Navigate to the project's root directory (one up from the Symfony installation folder)
+        $kernelDir = $this->params->get('kernel.project_dir');
+        $logFile = $kernelDir . '/../symfony-update.log';
+
+        $output->writeln('<info>Symfony Update Log File:</info> ' . $logFile);
+
+        if (!file_exists($logFile)) {
+            $io->writeln('<error>✗ log file not found</error>' . PHP_EOL);
+
+            ReportUtility::printEndTimestamp($io);
+            $io->writeln('[Report Failure]');
+            return Command::FAILURE;
+        }
+
+
+        $io->writeln('Reading events from symfony-update.log for the evaluation period:' . PHP_EOL);
+        $atLeastOneFound = false;
+
+        // Read file line by line
+        $handle = fopen($logFile, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                /*
+                 * All lines in the symfony-update.log should begin with the following prefix:
+                 * echo "$(date '+%Y-%m-%d %H:%M:%S') $(whoami) - ...."
+                 *
+                 * Thus, we can simply filter by the first part of the string to pull the month we want
+                 */
+
+                if (str_starts_with($line, $periodString)) {
+                    $output->writeln('- ' . $line);
+                    $atLeastOneFound = true;
+                }
+            }
+
+            fclose($handle);
+        }
+
+        if (!$atLeastOneFound) {
+            $io->writeln('✓ No results');
+        }
+
+
+        ReportUtility::printEndTimestamp($io);
         $io->writeln('[End of Report]');
 
         return Command::SUCCESS;
